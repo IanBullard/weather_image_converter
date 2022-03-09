98ab9b1e9f52811477f633f36de22880fd9145f1
Ian Bullard <ibullard@mjolnirstudios.com>
2022-03-09 17:34:26 -0600

    Floyd-Steinberg dithering algorithm

--- main.cpp
+++ main.cpp
@@ -85,6 +85,22 @@ void save_palette()
     FreeImage_Unload(image);
 }
 
+void convert_to_seven_color()
+{
+    /*
+    for each y from top to bottom do
+        for each x from left to right do
+            oldpixel := pixels[x][y]
+            newpixel := find_closest_palette_color(oldpixel)
+            pixels[x][y] := newpixel
+            quant_error := oldpixel - newpixel
+            pixels[x + 1][y    ] := pixels[x + 1][y    ] + quant_error × 7 / 16
+            pixels[x - 1][y + 1] := pixels[x - 1][y + 1] + quant_error × 3 / 16
+            pixels[x    ][y + 1] := pixels[x    ][y + 1] + quant_error × 5 / 16
+            pixels[x + 1][y + 1] := pixels[x + 1][y + 1] + quant_error × 1 / 16
+    */
+}
+
 int main(int argc, char** argv)
 {
     init_palette();
